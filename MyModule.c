beca7ed612119d4341254d653c872834443267c0
Oscar Chen <oscar.y.chen@gmail.com>
2021-11-09 11:52:25 -0700

    Added examples for raising Python exception and returning Python None value from C.

--- MyModule.c
+++ MyModule.c
@@ -1,7 +1,10 @@
 #include <Python.h>
  
-//  The function implemented in C
-int cFib(int n){
+/**
+ * The function implemented in C.
+ */
+int cFib(int n)
+{
     if (n<2){
         return n;
     } else {
@@ -9,15 +12,16 @@ int cFib(int n){
     }
 };
 
+
 /**
  * This function is called to translate arguments from Python.
  * The PyObject type is used as a pointer when we want to parse data between Python and C,
  * ie: static PyObject* myFunction(PyObject* self)
  * The PyArg_ParseTuple retrieves arguments from Python.
  * The Py_BuildValue turns a given value into Python object (PyObject pointer), see: https://docs.python.org/3/c-api/arg.html
- * If you wish to return nothing, return the Py_None value, which is equivalent to the Python None value.
  */
-static PyObject* fib(PyObject* self, PyObject* args){ 
+static PyObject* fib(PyObject* self, PyObject* args)
+{ 
     int n;
     if (!PyArg_ParseTuple(args, "i", &n)){
         return NULL;
@@ -25,12 +29,46 @@ static PyObject* fib(PyObject* self, PyObject* args){
     return Py_BuildValue("i", cFib(n));
 };
 
+
 /**
- * This function returns version information
+ * This function returns a string
  */
-static PyObject* version(PyObject* self){
+static PyObject* version(PyObject* self)
+{
     return Py_BuildValue("s", "Version 1.0");
 };
+
+
+/**
+ * This function demonstrates raising a Python exception from C function.
+ * See list of standard exceptions: https://docs.python.org/3/c-api/exceptions.html#standard-exceptions
+ * Note that you likely want to return NULL after setting a Python Exception.
+ */
+static PyObject* raise_exception(PyObject* self)
+{
+    PyErr_SetString(PyExc_Exception, "Oh no, something unexpected happened!");
+    return NULL;
+}
+
+
+/**
+ * This function demonstrates returning a Python None value.
+ */
+static PyObject* return_none(PyObject* self)
+{
+    return Py_BuildValue("");
+}
+
+
+/**
+ * This function demonstrates another way of returning a Python None value.
+ */
+static PyObject* return_none_2(PyObject* self)
+{
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
 
 /**
  * An array containing binding information for each function to be provided in this module.
@@ -41,22 +79,29 @@ static PyObject* version(PyObject* self){
  * - a description/documentation for the function.
  * The last entry must be a null entry, ie: {NULL, NULL, 0, NULL} which marks end of array.
  */
-static PyMethodDef myMethods[] = {
+static PyMethodDef myMethods[] = 
+{
     {"fib", fib, METH_VARARGS, "Calculates the fibonacci numbers."},
     {"version", (PyCFunction)version, METH_NOARGS, "Returns the version."},
+    {"raise_exception", (PyCFunction)raise_exception, METH_NOARGS, "Raises a Python exception."},
+    {"return_none", (PyCFunction)return_none, METH_NOARGS, "Returns a Python None value."},
+    {"return_none_2", (PyCFunction)return_none_2, METH_NOARGS, "Returns a Python None value."},
     {NULL, NULL, 0, NULL}
 };
+
 
 /**
  * Information about this module. 
  */
-static struct PyModuleDef myModule = {
+static struct PyModuleDef myModule = 
+{
     PyModuleDef_HEAD_INIT,
     "MyModule", // name of the module
-    "Fibonacci Module", // documentation of the module
+    "An example module to demonstrate Python C extension", // documentation of the module
     -1, // size of the preinterpreter state, -1 sets the module state to global
     myMethods // the PyMethodDef structure defined above
 };
+
 
 /**
  * Initializer function, this function is called when Python imports this module.
@@ -64,6 +109,7 @@ static struct PyModuleDef myModule = {
  * PyModule_Create creates the module from definition,
  * Initializes module and wraps the method pointers using module definition.
  */
-PyMODINIT_FUNC PyInit_MyModule(void){
+PyMODINIT_FUNC PyInit_MyModule(void)
+{
     return PyModule_Create(&myModule);
 }
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+# If you did not install the module, you can still import it if the module is
+# in the same directory as this file (copying the module from build/ folder).
 import MyModule
 
 print(MyModule.version())
 print(MyModule.fib(10))
+
+try:
+    MyModule.raise_exception()
+except Exception as e:
+    print(e)
+
+print(MyModule.return_none())
+print(MyModule.return_none_2())
